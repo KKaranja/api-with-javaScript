4fe31a20f1e609c535767954c14538d1ad03e160
Isaac Kimura <isaac.kimura@iprospect.com>
2022-01-14 00:04:31 +0300

    Project package installation and project set up

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const req = require("express/lib/request");
+const request = require("request-promise");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Amazon Scraper API.");
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
